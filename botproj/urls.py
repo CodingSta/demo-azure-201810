ac01773cae3c96dcbe4a20c55a57b915572a5063
Chinseok Lee <me@askcompany.kr>
2018-10-24 17:50:14 +0900

    photo ImageField 추가

--- botproj/urls.py
+++ botproj/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', post_list),
     path('shop/', item_list),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
