0ceb2c15001ca44a7c6cf42cf539ee65613f0901
Chinseok Lee <me@askcompany.kr>
2018-10-24 15:23:56 +0900

    shop, Item.name 필드에 최소길이 validator 적용

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,22 @@
+from django.core.validators import MinLengthValidator
+from django import forms
 from django.db import models
 
 
+# def min_length_3(value):
+#     if len(value) < 3:
+#         raise forms.ValidationError('3글자 이상 !!!')
+
+min_length_3 = MinLengthValidator(3)
+
+
+# TODO: Item.name 필드에 최대길이 10자 제한
+
+
+
 class Item(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,
+                            validators=[min_length_3])
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
