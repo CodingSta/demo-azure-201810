3a990dfe722c0938f81aa620c0f6dc3beea1a0d8
Chinseok Lee <me@askcompany.kr>
2018-10-24 12:36:39 +0900

    shop, item_list 뷰 구현

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def item_list(request):
+    # 데이터베이스로부터 모든 Item내역을 가져올려고 한다.
+    qs = Item.objects.all()
+    return render(request, 'shop/item_list.html', {
+        'item_list': qs,
+    })
