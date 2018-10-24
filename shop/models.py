6a0d16c9399f541096d49234cc38bddac6dbe509
Chinseok Lee <me@askcompany.kr>
2018-10-24 15:54:16 +0900

    shop, Item에 is_publish 필드 추가

--- shop/models.py
+++ shop/models.py
@@ -22,10 +22,10 @@ fn_20 = max_length_validator(20)
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=100,
-                            validators=[min_length_3, max_length_validator(10)])
+    name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
 
