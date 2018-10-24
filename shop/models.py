13cf2f52247f0ef8220e94415b8ee83ffc730134
Chinseok Lee <me@askcompany.kr>
2018-10-24 12:13:54 +0900

    shop앱 생성 및 Item모델 추가

---
+++ shop/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
