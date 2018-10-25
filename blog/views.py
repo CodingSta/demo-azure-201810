2dd4188a2dd8f1fcb6326b587ecd7392a69df665
Chinseok Lee <me@askcompany.kr>
2018-10-25 11:17:07 +0900

    blog, post_new 뷰 구현

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 
 
 # def post_list(request):
@@ -21,3 +22,21 @@ post_list = ListView.as_view(model=Post)
 #     })
 
 post_detail = DetailView.as_view(model=Post)
+
+
+# def post_new(request):
+#     if request.method == 'POST':
+#         form = PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             post = form.save()
+#             return redirect(f'/blog/{post.pk}')
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_form.html', {
+#         'form': form,
+#     })
+
+
+post_new = CreateView.as_view(model=Post,
+                              form_class=PostForm,
+                              success_url='/blog/')
--- botproj/urls.py
+++ botproj/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_new
 from shop.views import item_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', post_list),
     path('blog/<int:pk>/', post_detail),
+    path('blog/new/', post_new),
     path('shop/', item_list),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
